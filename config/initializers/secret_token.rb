c4e049f6e7c1cb96d0f933e7d92483689672bdc0
Ramiro <ramiro-r91@hotmail.com>
2013-08-16 00:55:23 -0300

    Nuevo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webestudio::Application.config.secret_token = '26c99975bf3f419767d673f520eec0deb588097182ea0118f6ea9bd81c005e645d8e22fa4840f20869754b23cd03906d2cee0afc98c7aa4da70d140f0ac6a4f8'
