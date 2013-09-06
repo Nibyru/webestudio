13dffadf1610d86d8e3cbf49e8300bcabdac2880
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-09-06 19:32:24 -0300

    [se elimino initiallizer carrierwave]

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-   config.dropbox_app_key = 'apvzqa7q9ukqdeb'
-   config.dropbox_app_secret = 'dqp5q9l2zw7fa9v'
-   config.dropbox_access_token = 'm55eqjdrqvai2gd6'
-   config.dropbox_access_token_secret = 'lkh5zjtuo09ewga'
-   config.dropbox_user_id = 54889672
-   config.dropbox_access_type = "app_folder"
-end
