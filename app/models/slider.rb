3a5a9c9eee63f8fa94a1e5ec85f15d60c7ade21a
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-09-07 01:53:49 -0300

    se modifico path de las imagenes

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,7 +1,7 @@
 class Slider < ActiveRecord::Base
   attr_accessible :title, :header, :photo
   
-  validates :title, :photo, presence: true
+  validates :title, presence: true
 
   has_attached_file :photo, styles: { slider: "1900x650>" }, path: ":style/:id_:filename"
 end
