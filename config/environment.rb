e48e83982839eac9616e0765a3f31514aa9e1f5a
PaulaLE <paula.le@live.com.ar>
2013-10-01 22:59:05 -0300

    se agrego sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Webestudio::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'cycestudiocontable.com.ar',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
