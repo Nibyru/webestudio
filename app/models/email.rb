fc392e7d0f20c09997d737558d064114960dc5bd
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-10-15 21:41:21 -0300

    se agrego email de contacto

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,6 +2,7 @@ class Email < ActionMailer::Base
   default from: 'contacto@cycestudiocontable.com'
 
   def send_email(contact)
+    @user = contact
     mail( to: contact.email, subject: 'Gracias por comunicarse' )
   end
 
