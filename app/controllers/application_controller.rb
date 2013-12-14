5348275d2ee8b2a9d11a7890108b37d27799cb6d
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-12-14 18:04:34 -0300

    cambios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
-	before_filter :data_collector
-  	protect_from_forgery
-  	
+
+  before_filter :data_collector
+
+  protect_from_forgery
+
 private
-	def data_collector
-		@links    = Link.all
-		@users    = User.all
-		@services = Service.all
-		@sliders  = Slider.all
-		@content  = Content.last || Content.new
-	end
+  def data_collector
+    @links    = Link.all
+    @users    = User.all
+    @services = Service.all
+    @sliders  = Slider.all
+    @contact  = Contact.new
+    @content  = Content.last || Content.new
+  end
 end
